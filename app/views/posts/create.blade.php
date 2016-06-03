ec2cdac6f2dad8b153e7ada10fb6bf8ad7ba27d6
Emartinez511 <mrblonde9993@gmail.com>
2016-06-03 15:52:17 -0500

    I fixed some syntax error and added some styling to this page.

--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -10,11 +10,10 @@
 @section('content')
 <h1>Create a Post</h1>
     {{ $errors->first('title', '<span class="help-block">TITLE: did not meet requirements</span>') }}
-    {{ $errors->first('description', '<span class="help-block">DESCRIPTION: did not meet requirements </span>') }}
     <div class="row">
-        {{ Form::open(array('action' => 'PostsController@store', 'class' => 'col s12')) }}
             <div class="row">
                 <div class="input-field col s12 m6 z-depth-1 #ffffff white">
+                    {{ Form::open(array('action' => 'PostsController@store', 'class' => 'col s12')) }}
                     {{Form::text('title', null, [ 'value' => "{{{ Input::old('title') }}}" ])}}
                     {{Form::label('title', 'Title')}}
                 </div>
@@ -23,6 +22,7 @@
                 <div class="input-field col s12 m8 z-depth-1 #ffffff white">
                     {{Form::textarea('description', null, ['value' => "{{{ Input::old('description') }}}", 'class' => 'materialize-textarea' ])}}
                     {{Form::label('description', 'Description')}}
+                    {{ $errors->first('description', '<span class="help-block">DESCRIPTION: did not meet requirements </span>') }}
                 </div>
             </div>
             {{ Form::submit('Post', null, ['class' => 'btn waves-effect waves-light z-depth-1' ])}}
