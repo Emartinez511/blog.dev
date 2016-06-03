90aa308c23f23d6b58fa09c8e944923245d1f9e5
Emartinez511 <mrblonde9993@gmail.com>
2016-06-03 09:40:00 -0500

    added basic layout for profile.

---
+++ app/views/portfolio.blade.php
@@ -0,0 +1,85 @@
+@extends('layouts.master')
+@section('material_script')
+    <!-- Compiled and minified CSS -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+      <!-- Compiled and minified JavaScript -->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+        <link rel="stylesheet" href="/css/main.css">
+
+@stop
+@section('content')
+<div class="section">
+
+<a class="title" href="">Portfolio</a>
+<div class="row isotope">
+    <div class="col s12 m12 l3">
+        <div class="card small">
+            <div class="card-image waves-effect waves-block waves-light">
+                <a href="">
+                    <img width="305" height="100" src="/img/simon_image.png" class="responsive-img wp-post-image" alt="" title="" />
+                </a>
+            </div>
+        <div class="card-content">
+            <p class="area"><a href="https://adbeus.com/coffee/montreal/cote-des-neiges/">Simple</a></p>
+            <a href="" ><span class="card-title activator brown-text text-darken-4">Simon</span></a>
+        </div>
+    </div>
+</div>
+<div class="col s12 m12 l3">
+        <div class="card small">
+            <div class="card-image waves-effect waves-block waves-light">
+                <a href="">
+                    <img src="/img/simon_image.png" class="responsive-img wp-post-image" alt="" title="" />
+                </a>
+            </div>
+        <div class="card-content">
+            <p class="area"><a href="https://adbeus.com/coffee/montreal/cote-des-neiges/">Simple</a></p>
+            <a href="" ><span class="card-title activator brown-text text-darken-4">Simon</span></a>
+        </div>
+    </div>
+</div>
+<div class="col s12 m12 l3">
+        <div class="card small">
+            <div class="card-image waves-effect waves-block waves-light">
+                <a href="">
+                    <img src="/img/simon_image.png" class="responsive-img wp-post-image" alt="" title="" />
+                </a>
+            </div>
+        <div class="card-content">
+            <p class="area"><a href="">Simple</a></p>
+            <a href="" ><span class="card-title activator brown-text text-darken-4">Simon</span></a>
+        </div>
+    </div>
+</div>
+</div>
+<div class="row isotope">
+<div class="col s12 m12 l3">
+        <div class="card small">
+            <div class="card-image waves-effect waves-block waves-light">
+                <a href="">
+                    <img src="/img/simon_image.png" class="responsive-img wp-post-image" alt="" title="" />
+                </a>
+            </div>
+        <div class="card-content">
+            <p class="area"><a href="">Simple</a></p>
+            <a href="" ><span class="card-title activator brown-text text-darken-4">Simon</span></a>
+        </div>
+    </div>
+</div>
+<div class="col s12 m12 l3">
+        <div class="card small">
+            <div class="card-image waves-effect waves-block waves-light">
+                <a href="">
+                    <img src="/img/simon_image.png" class="responsive-img wp-post-image" alt="" title="" />
+                </a>
+            </div>
+        <div class="card-content">
+            <p class="area"><a href="">Simple</a></p>
+            <a href="" ><span class="card-title activator brown-text text-darken-4">Simon</span></a>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+@stop
