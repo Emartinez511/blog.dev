a8bb87d967c70549e6b76cfd789e985af93bc878
Emartinez511 <mrblonde9993@gmail.com>
2016-06-03 09:37:49 -0500

    Added variables to methods.

---
+++ app/controllers/PostsController.php
@@ -0,0 +1,102 @@
+<?php
+
+class PostsController extends \BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		$posts = Post::all();
+		return View::make('posts.index')->with('posts', $posts);
+	}
+
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		return View::make('/posts/create');
+	}
+
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		$validator = Validator::make(Input::all(), Post::$rules);
+
+		    // attempt validation
+		    if ($validator->fails()) {
+		        // validation failed, redirect to the post create page with validation errors and old inputs
+		        return Redirect::back()->withInput()->withErrors($validator);
+		} else {
+			$post = new Post();
+			$post->title = Input::get('title');
+			$post->description = Input::get('description');
+			$post->user_id = User::first()->id;
+
+			if($post->save()) {
+				return Redirect::action('PostsController@show', $post->id);
+			}
+		}
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		$post = Post::find($id);
+		return View::make('posts.show')->with('post', $post);
+	}
+
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		return 'edit this specific post';
+	}
+
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+
+}
