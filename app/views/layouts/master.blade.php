d90de4e427c7abff7269c3596b4a979b9d2af921
Emartinez511 <mrblonde9993@gmail.com>
2016-06-06 11:34:50 -0500

    I added new navbar and successMessage.

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -3,22 +3,43 @@
 <head>
     <title>Laravel Blog</title>
     @yield("material_script")
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
     @section('navbar')
         <nav>
-            <div class="nav-wrapper z-depth-2 #90a4ae blue-grey lighten-2">
-              <a href="#" class="brand-logo"><img id="nav_logo" src="/img/biglogo.png"></a>
-              <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <div class="nav-wrapper z-depth-2 #90a4ae blue-grey lighten-2">
+            <a href="#" class="brand-logo"><img id="nav_logo" src="/img/biglogo.png"></a>
+            <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+            <ul class="right hide-on-med-and-down">
                 <li><a href="{{{ action('PostsController@index')}}}">HOME</a></li>
                 <li><a href="{{{ action('HomeController@portfolio')}}}">Portfolio</a></li>
                 <li><a href="{{{ action('HomeController@resume')}}}">Resume</a></li>
-              </ul>
-            </div>
+            </ul>
+            <ul class="side-nav" id="mobile-demo">
+                <li><a href="{{{ action('PostsController@index')}}}">HOME</a></li>
+                <li><a href="{{{ action('HomeController@portfolio')}}}">Portfolio</a></li>
+                <li><a href="{{{ action('HomeController@resume')}}}">Resume</a></li>
+            </ul>
+          </div>
         </nav>
     @show
 
 
 </head>
 <body class="#eceff1 blue-grey lighten-5">
+    @if (Session::has('successMessage'))
+        <div class="row">
+            <div class="m6 s6 col">
+                <div class="card-panel #e8f5e9 green lighten-5">
+                    <div class="row">
+                        <div class="col l8 white-text">
+                            <h6>{{{ Session::get('successMessage') }}}</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
     @yield('title')
     @yield('content')
 </body>
