5356a740332521ce56b440eb484c57b0bce13c9a
Emartinez511 <mrblonde9993@gmail.com>
2016-11-02 18:42:26 -0500

    added navbar

---
+++ app/views/partials/navbar.blade.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-default navbar-custom">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        {{-- <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li> --}}
+        <li><a href="#" class="nav-text">HOME</a></li>
+        <li><a href="#portfolio" class="nav-text">PORTFOLIO</a></li>
+        <li><a href="#resume" class="nav-text">RESUME</a></li>
+        <li><a href="https://www.linkedin.com/in/edricmartinez" class="nav-text">CONTACT ME</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
